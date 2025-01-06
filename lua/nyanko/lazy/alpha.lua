56cd8ae0bf5416338cabb3f28e8ff2035aac74c7
bunny winter <pyuku@pm.me>
2025-01-06 11:22:56 -0600

    add alpha.nvim start screen

---
+++ lua/nyanko/lazy/alpha.lua
@@ -0,0 +1,33 @@
+return {
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local theta = require("alpha.themes.theta")
+		local dashboard = require("alpha.themes.dashboard")
+
+		theta.header.val = {
+			"⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⢠⠏⡆⠀⠀⠀⠀⠀⢀⣀⣤⣤⣤⣀⡀     ",
+			"⠀⠀⠀⠀⠀⡟⢦⡀ ⠀⠀⣀⠞⠀⠀⠘⡀⢀⡠⠚⣉⠤⠂⠀⠀⠀⠈⠙⢦⡀   ",
+			"⠀⠀⠀⠀⠀⡇⠀⠉⠒⠊⠁⠀⠀⠀⠀⠀⠘⢧⠔⣉⠤⠒⠒⠉⠉⠀⠀⠀⠀⠹⣆  ",
+			"⠀⠀⠀⠀⠀⢰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠀⠀⣤⠶⠶⢶⡄⠀⠀⠀⠀⢹⡆",
+			"⠀⣀⠤⠒⠒⢺⠒⠀⠀⠀⠀⠀⠀⠀⠀⠤⠊⠀⢸⠀⡿⠀⡀⠀⣀⡟⠀⠀⠀⠀⢸⡇ ",
+			"⠈⠀⠀⣠⠴⠚⢯⡀⠐⠒⠚⠉⠀⢶⠂⠀⣀⠜⠀⢿⡀⠉⠚⠉⠀⠀⠀⠀⣠⠟   ",
+			"⠀⠠⠊⠀⠀⠀⠀⠙⠂⣴⠒⠒⣲⢔⠉⠉⣹⣞⣉⣈⠿⢦⣀⣀⣀⣠⡴⠟     ",
+		}
+
+		theta.buttons.val = {
+			{ type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+			{ type = "padding", val = 1 },
+			dashboard.button("e", "New file", "<cmd>ene<CR>"),
+			dashboard.button("<leader>ff", "Find file"),
+			dashboard.button("<leader>fg", "Live grep"),
+			dashboard.button("q", "Quit", "<cmd>qa<CR>"),
+		}
+
+		theta.file_icons.enabled = false
+
+		require("alpha").setup(theta.config)
+	end,
+}
