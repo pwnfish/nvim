48512ada1a20d9f005c2f9a76b15a95323ea49e1
bunny winter <pyuku@pm.me>
2025-01-12 12:04:09 -0600

    use which-key's helix preset instead of classic

--- lua/nyanko/lazy/which-key.lua
+++ lua/nyanko/lazy/which-key.lua
@@ -2,7 +2,12 @@ return {
 	"folke/which-key.nvim",
 	event = "VimEnter",
 	opts = {
+		preset = "helix",
+		win = {
+			padding = { 1, 2 },
+		},
 		icons = {
+			breadcrumb = "->",
 			separator = "->",
 			mappings = false,
 		},
