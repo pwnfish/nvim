91d6d37f497f69de98307d0c839bcf28ca81bb79
bunny winter <pyuku@pm.me>
2025-01-28 10:14:16 -0600

    fix incorrect keybinds on start screen

--- lua/nyanko/lazy/alpha.lua
+++ lua/nyanko/lazy/alpha.lua
@@ -18,8 +18,8 @@ return {
 			{ type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
 			{ type = "padding", val = 1 },
 			dashboard.button("e", "New file", "<cmd>ene<CR>"),
-			dashboard.button("<leader>ff", "Find file"),
-			dashboard.button("<leader>fg", "Live grep"),
+			dashboard.button("<leader>sf", "Search files"),
+			dashboard.button("<leader>sg", "Live grep"),
 			dashboard.button("q", "Quit", "<cmd>qa<CR>"),
 		}
 
