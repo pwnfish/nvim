a08d89bea5759d5401e9eee82eb845535cd0de66
bunny winter <pyuku@pm.me>
2025-02-11 20:34:02 -0600

    add telescope-file-browser

--- lua/nyanko/lazy/telescope.lua
+++ lua/nyanko/lazy/telescope.lua
@@ -5,13 +5,27 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope-ui-select.nvim",
+		"nvim-telescope/telescope-file-browser.nvim",
 	},
 	config = function()
 		require("telescope").setup({
+			defaults = {
+				theme = "center",
+				sorting_strategy = "ascending",
+				layout_config = {
+					horizontal = {
+						prompt_position = "top",
+						preview_width = 0.5,
+					},
+				},
+			},
 			extensions = {
 				["ui-select"] = {
 					require("telescope.themes").get_dropdown(),
 				},
+			},
+			file_browser = {
+				hijack_netrw = true,
 			},
 			pickers = {
 				find_files = {
@@ -21,11 +35,14 @@ return {
 		})
 
 		pcall(require("telescope").load_extension, "ui-select")
+		require("telescope").load_extension("file_browser")
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch by current [W]ord" })
 		vim.keymap.set("n", "<leader>sr", builtin.oldfiles, { desc = "[S]earch [R]ecent files" })
+
+		vim.keymap.set("n", "<leader>ob", ":Telescope file_browser<CR>", { desc = "[O]pen File [B]rowser" })
 	end,
 }
